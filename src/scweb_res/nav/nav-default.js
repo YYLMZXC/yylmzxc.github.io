4d11d88290a6518cc92c9bd4aae378f8c760defa
yylmzxc <yylmzxc@163.com>
2026-09-11 20:41:22 +0800

    2026-09-11 20:41:22

--- src/scweb_res/nav/nav-default.js
+++ src/scweb_res/nav/nav-default.js
@@ -325,6 +325,13 @@ window.SITE_NAV_DEFAULT = {
           "external": false
         },
         {
+          "id": "lmtwy2qoqf7ya",
+          "title": "yylmzxc-web",
+          "url": "yylmzxcweb/yylmzxc.html",
+          "external": true,
+          "key": ""
+        },
+        {
           "id": "about-tools-2",
           "key": "photoroomBg",
           "title": "Photoroom 透明背景",
